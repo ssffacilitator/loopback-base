1f60415f763e192a2fb9631fe9d569ecd1422d6a
Peter Horton <phorton@softstackfactory.org>
2020-01-11 14:50:18 -0800

    created mongo data source

---
+++ server/datasources.production.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongo: {
+        url: "mongodb+srv://phortonssf:pedro1234@cluster0-0vx31.mongodb.net/test?retryWrites=true&w=majority",
+        connector: 'mongodb',
+        name: "mongo"
+
+    }
+}
